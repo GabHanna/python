ae78dac2a25ea0cf648c2f10de709d7759dbf727
GabHanna <gabrielhannaa@gmail.com>
2024-09-04 21:07:59 -0300

    subindo versao 1 refatoracao2

---
+++ refatoracao2.py
@@ -0,0 +1,9 @@
+idade = 25
+
+categoria = "indefinida"
+if idade < 13:
+    categoria = "Crianca"
+elif idade >= 13 and idade < 18:
+    categoria = "Adolescente"
+
+print ("A pessoa é classificada como", categoria)
