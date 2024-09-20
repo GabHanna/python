5097fb353b21dcbaa438094e7a3f7901a055612e
GabHanna <gabrielhannaa@gmail.com>
2024-09-19 21:57:39 -0300

    Descricao do Commit

---
+++ exerc3.py
@@ -0,0 +1,29 @@
+##3
+for i in range(1, 21, 2):
+    print(i)
+
+##4
+for i in range(11):
+    if i % 2 == 0:
+        continue # Pula a interacao se i for impar
+    print(i)
+
+##5
+valor = 0
+numero = ""
+
+print("Insira um numero")
+
+while numero != 0:
+    valor = input("Digite o valor: (Digite 0 para sair) ")
+    soma += valor
+print(soma)
+
+##6
+
+
+
+
+
+
+
